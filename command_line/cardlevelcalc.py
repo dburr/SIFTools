e6111c274ae5af030b80cfb0dbaac79a1ed58ecd
Muh Muhten <muh.muhten@gmail.com>
2015-10-13 04:08:31 -0400

    refactor exp table lookups

--- command_line/cardlevelcalc.py
+++ command_line/cardlevelcalc.py
@@ -32,10 +32,39 @@ import getopt
 # data from: http://www59.atwiki.jp/lovelive-sif/pages/32.html
 # these have -1 as the first value because python arrays are 0-based and I am using <level> as the index
 # and am too lazy to do <level-1> to account for the 0-based-ness :P
-exp_table_n = [-1, 0, 6, 18, 28, 40, 51, 61, 72, 82, 93, 104, 114, 124, 135, 145, 156, 165, 176, 187, 196, 207, 217, 226, 238, 247, 257, 268, 277, 288, 297, 308, 317, 328, 337, 348, 358, 367, 377, 388, 397]
-exp_table_r = [-1, 0, 14, 31, 45, 55, 67, 76, 85, 94, 103, 110, 119, 125, 134, 140, 148, 155, 161, 168, 174, 181, 187, 193, 199, 206, 211, 217, 223, 228, 235, 240, 245, 251, 256, 262, 267, 272, 277, 283, 288, 292, 298, 303, 308, 313, 317, 323, 327, 332, 337, 342, 346, 351, 356, 360, 365, 370, 374, 378, 383]
-exp_table_sr = [-1, 0, 54, 98, 127, 150, 169, 187, 203, 218, 232, 245, 257, 269, 281, 291, 302, 311, 322, 331, 340, 349, 358, 366, 374, 383, 391, 398, 406, 413, 421, 428, 435, 442, 449, 456, 462, 469, 475, 482, 488, 494, 500, 507, 512, 518, 524, 530, 536, 541, 547, 552, 558, 563, 568, 574, 579, 584, 590, 594, 600, 605, 609, 615, 619, 625, 629, 634, 639, 643, 648, 653, 657, 662, 667, 670, 676, 680, 684, 689, 693]
-exp_table_ur = [-1, 0, 201, 294, 345, 382, 411, 438, 460, 481, 499, 517, 532, 547, 561, 574, 587, 598, 611, 621, 631, 642, 651, 661, 670, 679, 687, 696, 704, 712, 720, 727, 734, 742, 749, 755, 763, 769, 775, 782, 788, 794, 800, 806, 812, 818, 823, 829, 834, 840, 845, 850, 856, 860, 866, 870, 875, 880, 885, 890, 894, 899, 903, 908, 912, 917, 921, 925, 930, 933, 938, 942, 946, 950, 954, 959, 961, 966, 970, 974, 977, 981, 985, 988, 992, 996, 999, 1003, 1006, 1010, 1013, 1017, 1020, 1024, 1027, 1030, 1034, 1037, 1040, 1043, 1047]
+exp_tables = {
+        "N": [-1,
+            0, 6, 18, 28, 40, 51, 61, 72, 82, 93,
+            104, 114, 124, 135, 145, 156, 165, 176, 187, 196,
+            207, 217, 226, 238, 247, 257, 268, 277, 288, 297,
+            308, 317, 328, 337, 348, 358, 367, 377, 388, 397],
+        "R": [-1,
+            0, 14, 31, 45, 55, 67, 76, 85, 94, 103,
+            110, 119, 125, 134, 140, 148, 155, 161, 168, 174,
+            181, 187, 193, 199, 206, 211, 217, 223, 228, 235,
+            240, 245, 251, 256, 262, 267, 272, 277, 283, 288,
+            292, 298, 303, 308, 313, 317, 323, 327, 332, 337,
+            342, 346, 351, 356, 360, 365, 370, 374, 378, 383],
+        "SR": [-1,
+            0, 54, 98, 127, 150, 169, 187, 203, 218, 232,
+            245, 257, 269, 281, 291, 302, 311, 322, 331, 340,
+            349, 358, 366, 374, 383, 391, 398, 406, 413, 421,
+            428, 435, 442, 449, 456, 462, 469, 475, 482, 488,
+            494, 500, 507, 512, 518, 524, 530, 536, 541, 547,
+            552, 558, 563, 568, 574, 579, 584, 590, 594, 600,
+            605, 609, 615, 619, 625, 629, 634, 639, 643, 648,
+            653, 657, 662, 667, 670, 676, 680, 684, 689, 693],
+        "UR": [-1,
+            0, 201, 294, 345, 382, 411, 438, 460, 481, 499,
+            517, 532, 547, 561, 574, 587, 598, 611, 621, 631,
+            642, 651, 661, 670, 679, 687, 696, 704, 712, 720,
+            727, 734, 742, 749, 755, 763, 769, 775, 782, 788,
+            794, 800, 806, 812, 818, 823, 829, 834, 840, 845,
+            850, 856, 860, 866, 870, 875, 880, 885, 890, 894,
+            899, 903, 908, 912, 917, 921, 925, 930, 933, 938,
+            942, 946, 950, 954, 959, 961, 966, 970, 974, 977,
+            981, 985, 988, 992, 996, 999, 1003, 1006, 1010, 1013,
+            1017, 1020, 1024, 1027, 1030, 1034, 1037, 1040, 1043, 1047]}
 
 rarities = ["N", "R", "SR", "UR"]
 
@@ -49,26 +78,20 @@ def check_level_cap(rarity, level):
 
 def check_valid_exp(rarity, level, exp):
     return (rarity in rarities and check_level_cap(rarity, level)
-            and exp >= 0 and exp < exp_table_n[level])
+            and exp >= 0 and exp < exp_tables[rarity][level])
 
 def calc_exp_for_level(rarity, starting_level, starting_exp, desired_level):
     # assumes that all values fed into it have been checked already
     required_exp = 0
-    # desired_level+1 because python ranges are not inclusive :P
+    # desired_level+1 because python ranges are not inclusive on the RHS :P
     for level in range(starting_level+1, desired_level+1):
-        if rarity == "N":
-            required_exp = required_exp + exp_table_n[level]
-        elif rarity == "R":
-            required_exp = required_exp + exp_table_r[level]
-        elif rarity == "SR":
-            required_exp = required_exp + exp_table_sr[level]
-        elif rarity == "UR":
-            required_exp = required_exp + exp_table_ur[level]
+        required_exp += exp_tables[rarity][level]
         #print "WE ARE AT LEVEL %d and we need %d exp" % (level, required_exp)
     # subtract what we already have
-    required_exp = required_exp - starting_exp
+    required_exp -= starting_exp
     # now tell the user
-    print "To get a %s card from level %d (with %d EXP) to %d requires %d EXP." % (rarity, starting_level, starting_exp, desired_level, required_exp)
+    print "To get a %s card from level %d (with %d EXP) to %d requires %d EXP." \
+            % (rarity, starting_level, starting_exp, desired_level, required_exp)
     # calculate equivalent N cards (round up because we can't feed half of a card)
     number_of_n_cards = (required_exp // 100) + 1
     print "(the equivalent of about %d level-1 N cards fed to it)" % number_of_n_cards
@@ -77,17 +100,10 @@ def calc_level_for_exp(rarity, starting_level, starting_exp, level_for_exp):
     current_exp = starting_exp
     level_cap = level_caps[rarity]
     for level in range(starting_level+1, level_cap+1):
-        if rarity == "N":
-            current_exp = current_exp + exp_table_n[level]
-        elif rarity == "R":
-            current_exp = current_exp + exp_table_r[level]
-        elif rarity == "SR":
-            current_exp = current_exp + exp_table_sr[level]
-        elif rarity == "UR":
-            current_exp = current_exp + exp_table_ur[level]
+        current_exp += exp_tables[rarities][level]
         #print "WE ARE AT LEVEL %d and we need %d exp" % (level, required_exp)
         if current_exp > level_for_exp:
-            level = level - 1
+            level -= 1
             break
     if level > starting_level:
         if level == level_cap:
